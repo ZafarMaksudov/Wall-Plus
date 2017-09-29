b92323c95486f3a572c9bbb9986afb70085c8788
Yugansh Tyagi <yugansh39@gmail.com>
2017-09-29 07:05:11 +0530

    Downloaded Fragment functional *laggy*

--- app/src/main/java/ytstudios/wall/plus/DownloadFragmentAdapter.java
+++ app/src/main/java/ytstudios/wall/plus/DownloadFragmentAdapter.java
@@ -1,8 +1,101 @@
 package ytstudios.wall.plus;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
 /**
  * Created by Yugansh Tyagi on 29-09-2017.
  */
 
-public class DownloadFragmentAdapter {
+public class DownloadFragmentAdapter extends RecyclerView.Adapter {
+
+    private String[] paths;
+    private String[] names;
+    Context context;
+    DisplayMetrics displayMetrics;
+
+    public DownloadFragmentAdapter(Context context, String[] paths, String[] names) {
+        this.paths = paths;
+        this.names = names;
+        //this.activity = activity;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        RecyclerView.ViewHolder viewHolder;
+        displayMetrics = new DisplayMetrics();
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.downloaded_item, parent, false);
+        viewHolder = new DownloadsHolder(v, context);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        Bitmap bitmap = BitmapFactory.decodeFile(paths[position]);
+        int width = displayMetrics.widthPixels/2;
+        int height = 220;
+        Bitmap scaled = getResizedBitmap(bitmap, 320,220);
+
+        Uri uri = Uri.fromFile(new File(paths[position]));
+
+        Picasso.with(context).load(uri).into(((DownloadsHolder)holder).downloadedImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return paths.length;
+    }
+
+    public static class DownloadsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView downloadedImage;
+        Context context;
+
+        public DownloadsHolder(View itemView, Context context) {
+            super(itemView);
+            this.context = context;
+            downloadedImage = itemView.findViewById(R.id.downloadImage);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            Toast.makeText(this.context, "TOUCHED", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
+    {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // create a matrix for the manipulation
+        Matrix matrix = new Matrix();
+        // resize the bit map
+        matrix.postScale(scaleWidth, scaleHeight);
+        // recreate the new Bitmap
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
+    }
 }
--- app/src/main/java/ytstudios/wall/plus/Downloaded_Fragment.java
+++ app/src/main/java/ytstudios/wall/plus/Downloaded_Fragment.java
@@ -1,13 +1,20 @@
 package ytstudios.wall.plus;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.File;
 
 /**
  * Created by Yugansh Tyagi on 10-09-2017.
@@ -15,13 +22,71 @@ import android.view.ViewGroup;
 
 public class Downloaded_Fragment extends Fragment {
 
+    //GridView gridView;
+    RecyclerView recyclerView;
+    DownloadFragmentAdapter adapter;
+    Context context;
+    GridLayoutManager gridLayoutManager;
+
+    //Directories
+    private String[] filePaths;
+    private String[] fileNames;
+    private File[] files;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
 
         View view = inflater.inflate(R.layout.downloaded_fragment, null);
+
+        try{
+            File imageDir = new File(Environment.getExternalStorageDirectory().toString()+ "/Wall+/Wall+ Downloads");
+            Log.i("DIR", imageDir.toString());
+            if(imageDir.exists()){
+                files = imageDir.listFiles();
+                Log.i("FILES", String.valueOf(files.length));
+                filePaths = new String [files.length];
+                fileNames = new String [files.length];
+
+                for (int i = 0; i < files.length; i++) {
+                    // Get the path of the image file
+                    filePaths[i] = files[i].getAbsolutePath();
+                    Log.i("FILES", filePaths.toString());
+                    // Get the name image file
+                    fileNames[i] = files[i].getName();
+                    Log.i("FILES", fileNames.toString());
+                }
+
+            }
+        }
+        catch (Exception e){
+            Log.i("EXCEPTION ", e.toString());
+        }
+
+//
+//        gridView = view.findViewById(R.id.grid_view);
+//        adapter = new DownloadFragmentAdapter(getActivity(), filePaths, fileNames);
+//        gridView.setAdapter(adapter);
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(context, fileNames[i].toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+        gridLayoutManager = new GridLayoutManager(context,2);
+        recyclerView = view.findViewById(R.id.downloaded_rv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.addItemDecoration(new RecyclerItemDecoration(2));
+        recyclerView.setAdapter(new DownloadFragmentAdapter(getActivity().getApplicationContext(), filePaths, fileNames));
+
         return view;
     }
+
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
