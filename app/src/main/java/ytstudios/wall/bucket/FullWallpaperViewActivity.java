ab30286091ff5731456f51874ad500245ea103a3
Yugansh Tyagi <yugansh39@gmail.com>
2017-10-02 01:09:38 +0530

    Added Set wallpaper, AdMob not loaded

--- app/src/main/java/ytstudios/wall/bucket/FullWallpaperViewActivity.java
+++ app/src/main/java/ytstudios/wall/bucket/FullWallpaperViewActivity.java
@@ -1,40 +1,46 @@
 package ytstudios.wall.bucket;
 
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.like.LikeButton;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Yugansh Tyagi on 15-09-2017.
  */
 
-public class FullWallpaperViewActivity extends AppCompatActivity{
+public class FullWallpaperViewActivity extends AppCompatActivity {
 
     SimpleDraweeView full_imageView;
-    String encodedUrlFull,encodedUrlThumb,fileType;
+    String encodedUrlFull, encodedUrlThumb, fileType;
     int wallId;
-    AppBarLayout appBarLayout;
     Window window;
-    FloatingActionButton floatingActionButton;
-    LikeButton favToggleBtn;
     ViewPager fullScreenViewPager;
-    CharSequence options[] = new CharSequence[] {"Small - 1024x1024", "Medium - 2048x2048", "Large - 2732x2732"};
+    CharSequence options[] = new CharSequence[]{"Small - 1024x1024", "Medium - 2048x2048", "Large - 2732x2732"};
+
+    Button downloadWallBtn, setWallBtn;
+    CardView disableAdBlock;
 
     private AdView bannerAd;
 
@@ -44,13 +50,16 @@ public class FullWallpaperViewActivity extends AppCompatActivity{
 
         setContentView(R.layout.full_wallpaper_view_activity);
 
+        downloadWallBtn = findViewById(R.id.downloadWallBtn);
+        setWallBtn = findViewById(R.id.setWallBtn);
+        disableAdBlock = findViewById(R.id.disableAdBlock);
+
         window = getWindow();
-        if(Build.VERSION.SDK_INT >= 21){
+        if (Build.VERSION.SDK_INT >= 21) {
             window.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        else if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT){
+        } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
 
@@ -60,12 +69,24 @@ public class FullWallpaperViewActivity extends AppCompatActivity{
         bannerAd = new AdView(FullWallpaperViewActivity.this);
         bannerAd = findViewById(R.id.bannerAdView);
         AdRequest adRequest = new AdRequest.Builder()
-                                    .addTestDevice("02147518DD550E863FFAA08EA49B5F41")
-                                    .addTestDevice("4F18060E4B4A11E00C6E6C3B8EEF6353")
-                                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                                    .build();
+                .addTestDevice("02147518DD550E863FFAA08EA49B5F41")
+                .addTestDevice("4F18060E4B4A11E00C6E6C3B8EEF6353")
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
 
         bannerAd.loadAd(adRequest);
+        bannerAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdFailedToLoad(int i) {
+                disableAdBlock.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                disableAdBlock.setVisibility(View.GONE);
+            }
+        });
 
         encodedUrlFull = getIntent().getStringExtra("fullUrl");
         encodedUrlThumb = getIntent().getStringExtra("thumbUrl");
@@ -73,6 +94,13 @@ public class FullWallpaperViewActivity extends AppCompatActivity{
         wallId = getIntent().getIntExtra("id", 0);
         fileType = "." + getIntent().getStringExtra("file_type");
 
+
+        setWallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new setWall(getApplicationContext()).execute();
+            }
+        });
 
 //        floatingActionButton = findViewById(R.id.fab);
 //        floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -158,10 +186,10 @@ public class FullWallpaperViewActivity extends AppCompatActivity{
     }
 
 
-     class loadWall extends AsyncTask<String, Void, Uri>{
+    class loadWall extends AsyncTask<String, Void, Uri> {
 
-         @Override
-        protected Uri doInBackground(String ... params) {
+        @Override
+        protected Uri doInBackground(String... params) {
             Uri uri = Uri.parse(params[0]);
             return uri;
         }
@@ -177,6 +205,46 @@ public class FullWallpaperViewActivity extends AppCompatActivity{
             full_imageView.setImageURI(url);
         }
 
+    }
+
+    class setWall extends AsyncTask<Void, Void, Void> {
+
+        Context context;
+        Bitmap result;
+
+        public setWall(Context context) {
+            this.context = context;
+            this.result = null;
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                result = Picasso.with(FullWallpaperViewActivity.this)
+                        .load(encodedUrlFull)
+                        .get();
+            } catch (Exception e) {
+                Log.i("PICASSO EXCEPTION", e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(context, "Applying Wallpaper", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            WallpaperManager wallpaperManager = WallpaperManager.getInstance(FullWallpaperViewActivity.this);
+            try {
+                wallpaperManager.setBitmap(result);
+                Toast.makeText(context, "Wallpaper Applied Successfully", Toast.LENGTH_SHORT).show();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                Toast.makeText(context, "Cannot apply wallpaper!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
