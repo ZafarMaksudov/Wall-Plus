19b791431ab15d985088e8bd2ac8951cc273a746
Yugansh Tyagi <yugansh39@gmail.com>
2017-10-01 18:40:51 +0530

    Need to make Settings Preference Screen

--- app/src/main/java/ytstudios/wall/plus/SettingsPreference.java
+++ app/src/main/java/ytstudios/wall/plus/SettingsPreference.java
@@ -1,8 +1,21 @@
 package ytstudios.wall.plus;
 
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
 /**
  * Created by Yugansh Tyagi on 01-10-2017.
  */
 
-public class SettingsPreference {
+public class SettingsPreference extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preference_settings);
+    }
 }
